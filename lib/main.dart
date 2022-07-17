51f3cfb04b276fb7497bb99edd7c524606d4f887
Shreyas-Waghchaure <shreyasforlogin@gmail.com>
2022-07-17 15:33:26 +0530

    #3 fixed Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:templates/packages/login_screen/login.dart';
+import 'package:templates/packages/splash_screen/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginScreen(),
+      home: const splashScreen(),
     );
   }
 }
