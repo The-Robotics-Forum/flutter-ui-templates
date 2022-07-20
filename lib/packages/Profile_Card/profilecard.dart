bddfc5a7aa9c108c1502c6773ca9eedbb02dc835
Maruti54 <patilmaruti9604@gmail.com>
2022-07-20 16:53:48 +0530

    #33 Fixed Web View in profile card

--- lib/packages/Profile_Card/profilecard.dart
+++ lib/packages/Profile_Card/profilecard.dart
@@ -1,6 +1,4 @@
-import 'dart:developer';
-
-import 'package:flutter/material.dart';
+import 'dart:developer';import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
