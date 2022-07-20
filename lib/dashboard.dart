fc5666c771090d4351409d9e82264ca18865b052
Shreyas-Waghchaure <shreyasforlogin@gmail.com>
2022-07-20 20:40:49 +0530

    #30 fixed Search bar

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:templates/packages/Profile_Card/profilecard.dart';
 import 'package:templates/packages/bottom_navigation_bar/bottom_navigation_bar.dart';
@@ -18,17 +19,46 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    var _controller = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       drawer: appDrawer(),
       body:
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+          Scaffold(
+            backgroundColor: Colors.amberAccent,
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child:TextField(
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.black54,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
+                        hintText: 'Search Anything',
+                        hintStyle: TextStyle(color: Colors.white,fontSize: 17),
+                        prefixIcon: IconButton(icon: Icon(Icons.search,color: Colors.white,), onPressed: () { },),
+                        suffixIcon: IconButton(icon: Icon(Icons.clear ,color: Colors.white,),onPressed: (){_controller.clear();},),
+                       contentPadding: EdgeInsets.all(17),
+
+                      ),
+                      style:const TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                      ),
+                      cursorColor: Colors.white,
+                      cursorHeight: 17,
+                    ),
+                  ),
+
                 ElevatedButton(onPressed: (){Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const CartScreen()),
