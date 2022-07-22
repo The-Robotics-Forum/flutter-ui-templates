a1f13b866fca13d4fa908fa40d79868d3c2dc139
Maruti54 <patilmaruti9604@gmail.com>
2022-07-22 23:59:26 +0530

    updated

--- projects/ev_stations/lib/screens/users/ev_charging_stations.dart
+++ projects/ev_stations/lib/screens/users/ev_charging_stations.dart
@@ -1,1 +1,77 @@
 // ev_charging_stations
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Scanner extends StatefulWidget {
+  const Scanner({Key? key}) : super(key: key);
+
+  @override
+  _ScannerState createState() => _ScannerState();
+}
+
+class _ScannerState extends State<Scanner> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  late QRViewController controller;
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) async {
+      controller.pauseCamera();
+      if (await canLaunchUrl(Uri.parse((scanData.code)!))) {
+        await launchUrl(Uri.parse((scanData.code)!));
+        controller.resumeCamera();
+      } else {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Url'),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    Text('Barcode Type: ${describeEnum(scanData.format)}'),
+                    Text('Data: ${scanData.code}'),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('Ok'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    _launchInBrowser(Uri.parse((scanData.code)!));
+                  },
+                ),
+              ],
+            );
+          },
+        ).then((value) => controller.resumeCamera());
+      }
+    });
+  }
+}
+
+Future<void> _launchInBrowser(Uri url) async {
+  if (!await launchUrl(
+    url,
+    mode: LaunchMode.externalApplication,
+  )) {
+    throw 'Could not launch $url';
+  }
+}
