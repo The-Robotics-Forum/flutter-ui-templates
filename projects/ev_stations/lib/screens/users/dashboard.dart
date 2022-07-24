33510895282873c9055bfefe1a5c325e52110710
Maruti54 <patilmaruti9604@gmail.com>
2022-07-24 10:59:51 +0530

    #42 Fixed Dashboard

--- projects/ev_stations/lib/screens/users/dashboard.dart
+++ projects/ev_stations/lib/screens/users/dashboard.dart
@@ -128,172 +128,170 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ],
     ),
-      body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 30,
-              ),
-              Center(
-                child: Container(
-                  height: 100,
-                  width: 250,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF727171),
-                    border: Border.all(
-                      color: const Color(0xFF20BCDE),
-                    ),
-                    borderRadius: BorderRadius.circular(22),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: Container(
+                height: 100,
+                width: 250,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF727171),
+                  border: Border.all(
+                    color: const Color(0xFF20BCDE),
                   ),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10.0),
-                        child: RichText(
-                          text: const TextSpan(children: [
-                            WidgetSpan(
-                              child: Icon(
-                                Icons.account_balance_wallet_outlined,
-                                color: Color(0xFF20BCDE),
-                                size: 30,
-                              ),
+                  borderRadius: BorderRadius.circular(22),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: RichText(
+                        text: const TextSpan(children: [
+                          WidgetSpan(
+                            child: Icon(
+                              Icons.account_balance_wallet_outlined,
+                              color: Color(0xFF20BCDE),
+                              size: 30,
                             ),
-                            TextSpan(text: '  Wallet', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                          ]),
-                        ),
+                          ),
+                          TextSpan(text: '  Wallet', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                        ]),
                       ),
-                      const SizedBox(
-                        height: 20,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Center(
+                      child: Text(
+                        '\u{20B9}${'1023.23  |  36 XRP'}',
+                        style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
                       ),
-                      const Center(
-                        child: Text(
-                          '\u{20B9}${'1023.23  |  36 XRP'}',
-                          style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              Image.asset('assets/car1.png'),
-              const SizedBox(
-                height: 10,
-              ),
-              Text('Tata Nexon EV',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.white),),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Container(
-                          height: 50,
-                          width: (MediaQuery.of(context).size.width / 2)-30,
-                          decoration: BoxDecoration(
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Image.asset('assets/car1.png'),
+            const SizedBox(
+              height: 10,
+            ),
+            Text('Tata Nexon EV',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.white),),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Container(
+                        height: 50,
+                        width: (MediaQuery.of(context).size.width / 2)-30,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            BatteryBuild(batteryState),
+                            Text('$percentage Left')
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: 50,
+                        width: (MediaQuery.of(context).size.width / 2)-30,
+                        decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
                             color: Colors.white
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              BatteryBuild(batteryState),
-                              Text('$percentage Left')
-                            ],
-                          ),
                         ),
+                        child: Center(child: Text('${percentage*2} KM Left',style: const TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          height: 50,
-                          width: (MediaQuery.of(context).size.width / 2)-30,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white
-                          ),
-                          child: Center(child: Text('${percentage*2} KM Left',style: const TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 132,
+                    width: (MediaQuery.of(context).size.width / 2)-30,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Image.asset('assets/car2.jpeg',height: 100,),
                         ),
-                      )
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: 132,
-                      width: (MediaQuery.of(context).size.width / 2)-30,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white
-                      ),
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(4.0),
-                            child: Image.asset('assets/car2.jpeg',height: 100,),
-                          ),
-                          const Text('Find Charging Station')
-                        ],
-                      ),
+                        const Text('Find Charging Station')
+                      ],
                     ),
                   ),
-                  ]
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: 132,
-                      width: (MediaQuery.of(context).size.width / 2)-30,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white
-                      ),
-                      child: Column(
-                        children: const [
-                          Padding(
-                            padding: EdgeInsets.all(4.0),
-                            child: Icon(Icons.qr_code_scanner,size: 100,),
-                          ),
-                          Text('Scan QR Code')
-                        ],
-                      ),
+                ),
+                ]
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 132,
+                    width: (MediaQuery.of(context).size.width / 2)-30,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white
+                    ),
+                    child: Column(
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.all(4.0),
+                          child: Icon(Icons.qr_code_scanner,size: 100,),
+                        ),
+                        Text('Scan QR Code')
+                      ],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: 132,
-                      width: (MediaQuery.of(context).size.width / 2)-30,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white
-                      ),
-                      child: Column(
-                        children: const [
-                          Padding(
-                            padding: EdgeInsets.all(4.0),
-                            child: Icon(Icons.account_balance_wallet,size: 100,),
-                          ),
-                          Text('Add Money')
-                        ],
-                      ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 132,
+                    width: (MediaQuery.of(context).size.width / 2)-30,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white
+                    ),
+                    child: Column(
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.all(4.0),
+                          child: Icon(Icons.account_balance_wallet,size: 100,),
+                        ),
+                        Text('Add Money')
+                      ],
                     ),
                   ),
-                ],
-              )
-            ],
-          ),
+                ),
+              ],
+            )
+          ],
         ),
       ),// This trailing comma makes auto-formatting nicer for build methods.
     );
