3be3585c14dc393bb4a38df04a1d173d3183e75c
Maruti54 <patilmaruti9604@gmail.com>
2022-07-26 01:40:56 +0530

    #65 Fixed Stations Dashboard

--- projects/ev_stations/lib/screens/stations/station_dashboard.dart
+++ projects/ev_stations/lib/screens/stations/station_dashboard.dart
@@ -1,1 +1,189 @@
 // station_dashboard
+import 'package:flutter/material.dart';
+import 'package:battery_indicator/battery_indicator.dart';
+
+class StationDashboard extends StatefulWidget {
+  const StationDashboard({Key? key}) : super(key: key);
+
+  @override
+  State<StationDashboard> createState() => _StationDashboardState();
+}
+
+class _StationDashboardState extends State<StationDashboard> {
+
+  var _colorful = true;
+  var _showPercentSlide = true;
+  var _showPercentNum = false;
+  var _size = 40.0;
+  var _ratio = 3.0;
+  int batteryLevel=75;
+
+  Color _color = Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF454545),
+      ),
+      drawer: const Drawer(),
+      backgroundColor: const Color(0xFF454545),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          const Center(
+            child: Text(
+              'EV Charging Station',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Center(
+            child: RichText(
+              text: const TextSpan(children: [
+                WidgetSpan(
+                  child: Icon(
+                    Icons.location_pin,
+                    color: Color(0xFF20BCDE),
+                    size: 14,
+                  ),
+                ),
+                TextSpan(text: 'Upper Indira Nagar 666, Pune, Maharashtra'),
+              ]),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Center(
+            child: Container(
+              height: 40,
+              width: 250,
+              decoration: BoxDecoration(
+                  border: Border.all(color: const Color(0xFF20BCDE)),
+                  borderRadius: BorderRadius.circular(22)),
+              child: const Center(
+                child: Text(
+                  'Charger Type | CCS4',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  '\u{20B9}${'99/Kwh'}',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  width: 50,
+                ),
+                Text(
+                  '3 XPR/kwh',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('$batteryLevel %',style: const TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),
+              const SizedBox(
+                width: 10,
+              ),
+              BatteryIndicator(
+                batteryFromPhone: false,
+                batteryLevel: 75,
+                style: BatteryIndicatorStyle.skeumorphism,
+                colorful: _colorful,
+                showPercentNum: _showPercentNum,
+                mainColor: _color,
+                size: _size,
+                ratio: _ratio,
+                showPercentSlide: _showPercentSlide,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            height: 50,
+            width: 300,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10)),
+            child: const Center(
+              child: Text('Statistics',
+                  style: TextStyle(
+                    fontSize: 20,
+                    letterSpacing: 5.0,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            height: 50,
+            width: 300,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10)),
+            child: const Center(
+              child: Text('Prediction',
+                  style: TextStyle(
+                    fontSize: 20,
+                    letterSpacing: 5.0,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            height: 50,
+            width: 300,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10)),
+              child: const Center(
+                child: Text('Request Power',
+                  style: TextStyle(
+                    fontSize: 20,
+                    letterSpacing: 5.0,
+                    fontWeight: FontWeight.bold,
+                  )),
+              ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- projects/ev_stations/lib/screens/users/drawer.dart
+++ projects/ev_stations/lib/screens/users/drawer.dart
@@ -2,6 +2,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+
 class DrawerScreen extends StatefulWidget {
   const DrawerScreen({Key? key}) : super(key: key);
 
@@ -14,7 +15,7 @@ class _DrawerScreenState extends State<DrawerScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF454545),
-      appBar:  AppBar(
+      appBar: AppBar(
         toolbarHeight: 60,
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -39,17 +40,22 @@ class _DrawerScreenState extends State<DrawerScreen> {
           ),
         ],
       ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-
-          children: [
-            Container(alignment:Alignment.center,child: const Text('Below button shows the PopUp Drawer ',style: TextStyle(color: Colors.white,fontSize: 30),)),
-
-            Center(
-              child: ElevatedButton(
-                child: const Text('Open Drawer'),
-                onPressed: (){
-                  showDialog(context: context, builder: (context)=>  BackdropFilter(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+              alignment: Alignment.center,
+              child: const Text(
+                'Below button shows the PopUp Drawer ',
+                style: TextStyle(color: Colors.white, fontSize: 30),
+              )),
+          Center(
+            child: ElevatedButton(
+              child: const Text('Open Drawer'),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => BackdropFilter(
                     filter: ImageFilter.blur(
                       sigmaX: 3.0,
                       sigmaY: 3.0,
@@ -57,72 +63,89 @@ class _DrawerScreenState extends State<DrawerScreen> {
                     child: SimpleDialog(
                       insetPadding: EdgeInsets.zero,
                       shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)
-                      ),
-                      children:[
+                          borderRadius: BorderRadius.circular(20)),
+                      children: [
                         Container(
                           height: 300,
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.center,
                             mainAxisAlignment: MainAxisAlignment.center,
-                            children:  [
-                               SimpleDialogOption(
+                            children: [
+                              SimpleDialogOption(
                                 child: InkWell(
-                                  onTap:(){},
+                                  onTap: () {},
                                   child: const Padding(
                                     padding: EdgeInsets.all(15.0),
-                                    child: Text('Transaction Log',style: TextStyle(color: Colors.white,fontSize: 25),),
+                                    child: Text(
+                                      'Transaction Log',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 25),
+                                    ),
                                   ),
                                 ),
                               ),
-                              const Divider( color: Colors.white, height: 1, endIndent: 7),
+                              const Divider(
+                                  color: Colors.white, height: 1, endIndent: 7),
                               InkWell(
-                                onTap: (){},
-                                child: const Padding(
-                                  padding: EdgeInsets.all(15.0),
-                                  child:  SimpleDialogOption(
-                                    child: Text('Update Price',style:  TextStyle(color: Colors.white,fontSize: 25),),
-                                  ),
-                                ),
-                              ),
-                              const Divider(color: Colors.white, height: 1, endIndent: 7),
-                              InkWell(
-                                onTap: (){},
+                                onTap: () {},
                                 child: const Padding(
                                   padding: EdgeInsets.all(15.0),
                                   child: SimpleDialogOption(
-                                    child: Text('Edit Profile',style: TextStyle(color: Colors.white,fontSize: 25),),
+                                    child: Text(
+                                      'Update Price',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 25),
+                                    ),
                                   ),
                                 ),
                               ),
-                              const Divider(color: Colors.white, height: 1, endIndent: 7),
+                              const Divider(
+                                  color: Colors.white, height: 1, endIndent: 7),
                               InkWell(
-                                onTap: (){},
+                                onTap: () {},
                                 child: const Padding(
-                                  padding:  EdgeInsets.all(8.0),
-                                  child:  SimpleDialogOption(
-                                    child: Text('Log Out',style: TextStyle(color: Colors.redAccent,fontSize: 25),),
+                                  padding: EdgeInsets.all(15.0),
+                                  child: SimpleDialogOption(
+                                    child: Text(
+                                      'Edit Profile',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 25),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const Divider(
+                                  color: Colors.white, height: 1, endIndent: 7),
+                              InkWell(
+                                onTap: () {},
+                                child: const Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: SimpleDialogOption(
+                                    child: Text(
+                                      'Log Out',
+                                      style: TextStyle(
+                                          color: Colors.redAccent,
+                                          fontSize: 25),
+                                    ),
                                   ),
                                 ),
                               ),
                             ],
                           ),
-
                         ),
                       ],
                       backgroundColor: const Color(0xFF454545),
-                      contentPadding: const EdgeInsets.only(left: 10,right: 10),
+                      contentPadding:
+                          const EdgeInsets.only(left: 10, right: 10),
                       elevation: 0,
-                      ),
+                    ),
                   ),
-
-
-                  );
-                },
-              ),
+                );
+              },
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 }
