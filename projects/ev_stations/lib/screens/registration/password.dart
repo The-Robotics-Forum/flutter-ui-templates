ef6fb3814df23e179ad297ec3dfa722dcbb0e76f
Maruti54 <patilmaruti9604@gmail.com>
2022-07-29 13:21:30 +0530

    #78 Fixed Verification Screen

--- projects/ev_stations/lib/screens/registration/password.dart
+++ projects/ev_stations/lib/screens/registration/password.dart
@@ -1,1 +1,97 @@
 // password
+// verification
+// Login Screen
+import 'package:ev_stations/screens/users/dashboard.dart';
+import 'package:flutter/material.dart';
+
+class Password extends StatefulWidget {
+  const Password({Key? key}) : super(key: key);
+
+  @override
+  _PasswordState createState() => _PasswordState();
+}
+
+class _PasswordState extends State<Password> {
+  // ignore: prefer_typing_uninitialized_variables
+  var _value;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF454545),
+      appBar: AppBar(
+        leading: InkWell(onTap: (){Navigator.pop(context);},child:  Icon(Icons.arrow_back)),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+              child: Text(
+                'Create a Password',
+                style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 25, right: 25),
+              height: 35,
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(22.0), borderSide: const BorderSide(color: Colors.grey) ,),
+                  focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(22.0), borderSide: const BorderSide(color: Colors.grey) ,),
+                  filled: true,
+                  contentPadding: const EdgeInsets.only(top: 5, left: 35),
+                  hintText: 'Enter a strong password',
+                  hintStyle:  TextStyle(color: Colors.grey),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 25, right: 25),
+              height: 35,
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(22.0), borderSide: const BorderSide(color: Colors.grey) ,),
+                  focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(22.0), borderSide: const BorderSide(color: Colors.grey) ,),
+                  filled: true,
+                  contentPadding: const EdgeInsets.only(top: 5, left: 35),
+                  hintText: 'Renter the password',
+                  hintStyle:  TextStyle(color: Colors.grey),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: (){
+
+              },
+              child: Center(
+                child: Container(
+                  width: 150,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: const Color(0xFF20BCDE),
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: const Center(child: Text('Set',style: TextStyle(color: Colors.white,fontSize: 28),)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
