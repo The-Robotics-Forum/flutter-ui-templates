fb2724127a0f756e58a82e8844bc39c6dae32877
Maruti Patil <90641815+Maruti54@users.noreply.github.com>
2022-07-29 23:38:23 +0530

    Register User & Station Screen (#81)
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #78 Fixed Verification Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen

--- projects/ev_stations/lib/screens/registration/register_station.dart
+++ projects/ev_stations/lib/screens/registration/register_station.dart
@@ -1,1 +1,170 @@
-// register_sation
+// register_station
+import 'package:flutter/material.dart';
+
+class RegisterStation extends StatefulWidget {
+  const RegisterStation({Key? key}) : super(key: key);
+
+  @override
+  _RegisterStationState createState() => _RegisterStationState();
+}
+
+class _RegisterStationState extends State<RegisterStation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF454545),
+      appBar: AppBar(
+        leading: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(Icons.arrow_back)),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+              child: Text(
+                'Hello Pavan :)',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 25, right: 25),
+                  height: 35,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22.0),
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22.0),
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
+                      filled: true,
+                      contentPadding: const EdgeInsets.only(top: 5, left: 35),
+                      hintText: 'Station Name',
+                      hintStyle: const TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 25, right: 25),
+                  height: 35,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        icon: const Icon(
+                          Icons.arrow_drop_down_outlined,
+                          color: Colors.grey,
+                        ),
+                        onPressed: () {},
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22.0),
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22.0),
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
+                      filled: true,
+                      contentPadding: const EdgeInsets.only(top: 5, left: 35),
+                      hintText: 'Charging Types',
+                      hintStyle: const TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 25, right: 25),
+                  height: 35,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22.0),
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22.0),
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
+                      filled: true,
+                      contentPadding: const EdgeInsets.only(top: 5, left: 35),
+                      hintText: 'No. of Plug',
+                      hintStyle: const TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 25, right: 25),
+                  height: 35,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22.0),
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22.0),
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
+                      filled: true,
+                      contentPadding: const EdgeInsets.only(top: 5, left: 35),
+                      hintText: 'Rate',
+                      hintStyle: const TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Center(
+                child: Container(
+                  width: 150,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: const Color(0xFF20BCDE),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: const Center(
+                      child: Text(
+                    'Save',
+                    style: TextStyle(color: Colors.white, fontSize: 28),
+                  )),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- projects/ev_stations/lib/screens/registration/register_user.dart
+++ projects/ev_stations/lib/screens/registration/register_user.dart
@@ -1,1 +1,134 @@
 // register_user
+import 'package:ev_stations/screens/registration/register_station.dart';
+import 'package:flutter/material.dart';
+
+class RegisterUser extends StatefulWidget {
+  const RegisterUser({Key? key}) : super(key: key);
+
+  @override
+  _RegisterUserState createState() => _RegisterUserState();
+}
+
+class _RegisterUserState extends State<RegisterUser> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF454545),
+      appBar: AppBar(
+        leading: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(Icons.arrow_back)),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+              child: Text(
+                'Hello Siddhant :)',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 25, right: 25),
+                  height: 35,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        icon: const Icon(
+                          Icons.arrow_drop_down_outlined,
+                          color: Colors.grey,
+                        ),
+                        onPressed: () {},
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22.0),
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22.0),
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
+                      filled: true,
+                      contentPadding: const EdgeInsets.only(top: 5, left: 35),
+                      hintText: 'Car Model',
+                      hintStyle: const TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 25, right: 25),
+                  height: 35,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        icon: const Icon(
+                          Icons.arrow_drop_down_outlined,
+                          color: Colors.grey,
+                        ),
+                        onPressed: () {},
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22.0),
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(22.0),
+                        borderSide: const BorderSide(color: Colors.grey),
+                      ),
+                      filled: true,
+                      contentPadding: const EdgeInsets.only(top: 5, left: 35),
+                      hintText: 'Plug Type',
+                      hintStyle: const TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>  const RegisterStation()));
+              },
+              child: Center(
+                child: Container(
+                  width: 150,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: const Color(0xFF20BCDE),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: const Center(
+                      child: Text(
+                    'Save',
+                    style: TextStyle(color: Colors.white, fontSize: 28),
+                  )),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
