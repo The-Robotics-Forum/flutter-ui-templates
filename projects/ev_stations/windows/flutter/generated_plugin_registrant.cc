b1cb671310ab86d1fb37741ccb6061b722db7989
Shreyas Waghchaure <72643976+Shreyas-Waghchaure@users.noreply.github.com>
2022-07-30 11:04:39 +0530

    #77 Fixed Registration Screen (#89)

--- projects/ev_stations/windows/flutter/generated_plugin_registrant.cc
+++ projects/ev_stations/windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,14 @@
 #include "generated_plugin_registrant.h"
 
 #include <battery_plus_windows/battery_plus_windows_plugin.h>
+#include <geolocator_windows/geolocator_windows.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   BatteryPlusWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("BatteryPlusWindowsPlugin"));
+  GeolocatorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("GeolocatorWindows"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
