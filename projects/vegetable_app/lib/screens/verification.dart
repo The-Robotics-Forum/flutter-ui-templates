f25f87acef2ccb01cefce6fe1153fbc674d1eace
Maruti54 <patilmaruti9604@gmail.com>
2022-07-30 14:31:39 +0530

    #85 Fixed Password and Verification screen

---
+++ projects/vegetable_app/lib/screens/verification.dart
@@ -0,0 +1,102 @@
+// verification
+import 'password.dart';
+import 'package:flutter/material.dart';
+
+class Verification extends StatefulWidget {
+  const Verification({Key? key}) : super(key: key);
+
+  @override
+  _VerificationState createState() => _VerificationState();
+}
+
+class _VerificationState extends State<Verification> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white70,
+      appBar: AppBar(
+        leading: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(Icons.arrow_back,color: Colors.black,)),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+              child: Text(
+                'Verification',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+              child: Text(
+                'OTP sent on XXXX-XXXX-54',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 25, right: 25),
+              height: 35,
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(22.0),
+                    borderSide: const BorderSide(color: Colors.grey),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: const EdgeInsets.only(top: 5, left: 35),
+                  hintText: 'Enter OTP',
+                  hintStyle: const TextStyle(color: Colors.grey),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Password()));
+              },
+              child: Center(
+                child: Container(
+                  width: 150,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: const Color(0xFF20BCDE),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: const Center(
+                      child: Text(
+                        'Verify',
+                        style: TextStyle(color: Colors.white, fontSize: 28),
+                      )),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
