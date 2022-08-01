3166576fef9740d570c201d68fa084410c1e3aae
Shreyas Waghchaure <72643976+Shreyas-Waghchaure@users.noreply.github.com>
2022-08-01 22:58:35 +0530

    Language selection Screen (#93)
    * #84 Fixed Registration Screen
    
    * #82 Fixed Language Selection Screen

--- projects/vegetable_app/lib/main.dart
+++ projects/vegetable_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:vegetable_app/screens/dashboard.dart';
+import 'package:vegetable_app/screens/register.dart';
+import 'package:vegetable_app/screens/select_language.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,7 +28,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const Dashboard(),
+      home: const SelectLanguage(),
     );
   }
 }
