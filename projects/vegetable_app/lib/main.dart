eb35f9b748ffac9bf92e4adf074ef7c863679434
Shreyas-Waghchaure <shreyasforlogin@gmail.com>
2022-08-04 12:29:40 +0530

    #88 Fixed Notification Screen

--- projects/vegetable_app/lib/main.dart
+++ projects/vegetable_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vegetable_app/screens/dashboard.dart';
+import 'package:vegetable_app/screens/notification.dart';
 import 'package:vegetable_app/screens/register.dart';
 import 'package:vegetable_app/screens/select_language.dart';
 import 'package:vegetable_app/screens/vege_info.dart';
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const VegeInfoScreen(),
+      home: const NotificationScreen(),
     );
   }
 }
