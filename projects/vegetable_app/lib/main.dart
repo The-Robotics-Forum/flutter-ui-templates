a2d882ae2db8e32d8e34b7d136ce165eb6dc6aa4
Shreyas-Waghchaure <shreyasforlogin@gmail.com>
2022-08-08 11:53:57 +0530

    #103 Fixed Timeline of Vegetable

--- projects/vegetable_app/lib/main.dart
+++ projects/vegetable_app/lib/main.dart
@@ -1,9 +1,14 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:vegetable_app/screens/best_farmers.dart';
 import 'package:vegetable_app/screens/dashboard.dart';
 import 'package:vegetable_app/screens/notification.dart';
 import 'package:vegetable_app/screens/register.dart';
 import 'package:vegetable_app/screens/select_language.dart';
+import 'package:vegetable_app/screens/Timeline.dart';
+
+
 import 'package:vegetable_app/screens/vege_info.dart';
 
 void main() {
@@ -31,7 +36,9 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const BestFarmersScreen(),
+
+      home: const TimelineScreen(),
+
     );
   }
 }
