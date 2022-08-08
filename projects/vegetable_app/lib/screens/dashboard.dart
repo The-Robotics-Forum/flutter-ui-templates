50d13dacd2a7a1ba330d3f3cf7e821ff19e9b5e8
Maruti Patil <90641815+Maruti54@users.noreply.github.com>
2022-08-08 14:04:24 +0530

    Statistics Screen (#110)
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #78 Fixed Verification Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #78 Fixed Verification Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #79 Fixed Register User & Station Screen
    
    * #86 Fixed Dashboard Screen
    
    * #86 Fixed Dashboard Screen
    
    * #86 Fixed Dashboard Screen
    
    * #85 Fixed Password and Verification screen
    
    * #85 Fixed Password and Verification screen
    
    * #83 Fixed Login Screen
    
    * #100 Fixed Statistics Screen

--- projects/vegetable_app/lib/screens/dashboard.dart
+++ projects/vegetable_app/lib/screens/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tab_container/tab_container.dart';
+import 'package:vegetable_app/screens/statistics_screen.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -108,7 +109,7 @@ class _DashboardState extends State<Dashboard> {
                     ),
                     Container(),
                     Container(),
-                    Container()
+                    Statistics(),
                   ],
                 ),
               ),
