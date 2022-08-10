7288ffff6f5e3b34f7edfcbc911e97bfb6ead15b
Maruti54 <patilmaruti9604@gmail.com>
2022-08-10 18:58:32 +0530

    #105 Fixed Dealer Screens

--- projects/vegetable_app/lib/main.dart
+++ projects/vegetable_app/lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const DealerScreen(),
+      home: const LoginScreen(),
     );
   }
 }
